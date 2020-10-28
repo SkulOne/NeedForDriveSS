c1a121afdc9a1b65303336a6d02bf4d05b20781a
Sultan <wndrngr13@gmail.com>
2020-10-28 20:40:52 +0400

    fix: path to img

--- src/app/shared/entities/slides.ts
+++ src/app/shared/entities/slides.ts
@@ -5,21 +5,21 @@ export const slides: Slide[] = [
         title: 'Бесплатная парковка',
         subtitle:
             'Оставляйте машину на платных городских парковках и разрешенных местах, не нарушая ПДД, а также в аэропортах.',
-        img: "url('../../../../../../assets/images/slide1.png')",
+        img: "url('assets/images/slide1.png')",
     },
     {
         title: 'Страховка',
         subtitle: 'Полная страховка страховка автомобиля',
-        img: "url('../../../../../../assets/images/slide2.png')",
+        img: "url('assets/images/slide2.png')",
     },
     {
         title: 'Бензин',
         subtitle: 'Полный бак на любой заправке города за наш счёт',
-        img: "url('../../../../../../assets/images/slide3.png')",
+        img: "url('assets/images/slide3.png')",
     },
     {
         title: 'Обслуживание',
         subtitle: 'Автомобиль проходит еженедельное ТО',
-        img: "url('../../../../../../assets/images/slide4.png')",
+        img: "url('assets/images/slide4.png')",
     },
 ];
