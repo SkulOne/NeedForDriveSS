0b84efff3e384dd12bb1d88a80a88855b4ea38bf
Sultan <wndrngr13@gmail.com>
2020-11-15 23:09:10 +0400

    feat: add validator for location

--- src/app/modules/order/components/location/location.component.ts
+++ src/app/modules/order/components/location/location.component.ts
@@ -1,5 +1,5 @@
 import { ChangeDetectionStrategy, Component, Input, OnDestroy, OnInit } from '@angular/core';
-import { AbstractControl } from '@angular/forms';
+import { AbstractControl, ValidationErrors, ValidatorFn } from '@angular/forms';
 import { LocationService } from '../../../../shared/services/location.service';
 import { mapStyle } from './mapStyle';
 import { Observable, of } from 'rxjs';
@@ -11,6 +11,7 @@ import MapTypeStyle = google.maps.MapTypeStyle;
 import LatLngLiteral = google.maps.LatLngLiteral;
 import LatLng = google.maps.LatLng;
 import { untilDestroyed } from 'ngx-take-until-destroy';
+import { MatOptionSelectionChange } from '@angular/material/core';
 
 @Component({
   selector: 'app-location',
@@ -38,13 +39,24 @@ export class LocationComponent implements OnInit, OnDestroy {
     this.coords$ = this.locationService.getUserCoords();
 
     this.cities$ = this.locationService.getAllCity();
+    this.cities$.pipe(untilDestroyed(this)).subscribe((cities) => {
+      const citiesName = cities.map((city) => city.name);
+      this.locationForm.get('city').setValidators(autocompleteValidator(citiesName));
+    });
 
     this.clear();
   }
 
-  citySelected(city: City): void {
-    this.coords$ = this.locationService.getCoordsByAddress(city.name);
-    this.points$ = this.pointService.getPointsFormCity(city.id);
+  citySelected(city: City, event: MatOptionSelectionChange): void {
+    if (event.isUserInput) {
+      this.coords$ = this.locationService.getCoordsByAddress(city.name);
+      this.points$ = this.pointService.getPointsFormCity(city.id);
+
+      this.points$.pipe(untilDestroyed(this)).subscribe((points) => {
+        const pointsName = points.map((point) => point.address);
+        this.locationForm.get('pickupPoint').setValidators(autocompleteValidator(pointsName));
+      });
+    }
   }
 
   clear(): void {
@@ -74,4 +86,13 @@ export class LocationComponent implements OnInit, OnDestroy {
   }
 
   ngOnDestroy(): void {}
+}
+
+function autocompleteValidator(validOptions: string[]): ValidatorFn {
+  return (control: AbstractControl): ValidationErrors | null => {
+    if (validOptions.indexOf(control.value) !== -1) {
+      return null;
+    }
+    return { invalidAutocomplete: true };
+  };
 }
