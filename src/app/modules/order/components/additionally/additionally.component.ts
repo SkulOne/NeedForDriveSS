c763a10a9fb4b0ceac079f18fc137c8bf0019b06
Sultan <wndrngr13@gmail.com>
2020-11-18 21:59:59 +0400

    refactor: put inputs

--- src/app/modules/order/components/additionally/additionally.component.ts
+++ src/app/modules/order/components/additionally/additionally.component.ts
@@ -1,7 +1,7 @@
 import { Component, ChangeDetectionStrategy, Input } from '@angular/core';
 import { AbstractControl } from '@angular/forms';
-import { InputCheckedElement, TextInput } from '../../../../shared/interfaces/input';
 import { IDatePickerConfig } from 'ng2-date-picker';
+import { colorInputs, dateInput, serviceInputs, additionalInputs } from './additionnallyInputs';
 
 @Component({
   selector: 'app-additionally',
@@ -13,24 +13,11 @@ export class AdditionallyComponent {
   @Input() additionallyForm: AbstractControl;
   maskedVisible = false;
 
-  colorInputs: InputCheckedElement[] = [
-    { id: 'any', value: 'Любой', labelValue: 'Любой' },
-    { id: 'Red', value: 'Красный', labelValue: 'Красный' },
-    { id: 'Blue', value: 'Синий', labelValue: 'Синий' },
-  ];
-  serviceInputs: InputCheckedElement[] = [
-    { id: 'minute', value: 'поминутно', labelValue: 'Поминутно, 7Р/мин' },
-    { id: 'day', value: 'суточно', labelValue: 'На стуки, 1999Р/сутки' },
-  ];
-  additionalInputs: InputCheckedElement[] = [
-    { id: 'tank', value: 'бак', labelValue: 'Полный бак, 500р' },
-    { id: 'armchair', value: 'кресло', labelValue: 'Детское кресло, 200р' },
-    { id: 'right', value: 'праворульный', labelValue: 'Правый руль, 1600р' },
-  ];
-  dateInput: TextInput[] = [
-    { label: 'C', controlName: 'startDate', matLabel: 'Дата начала аренды' },
-    { label: 'По', controlName: 'endDate', matLabel: 'Дата конца аренды' },
-  ];
+  colorInputs = colorInputs;
+  dateInput = dateInput;
+  serviceInputs = serviceInputs;
+  additionalInputs = additionalInputs;
+
   datePickerConfig: IDatePickerConfig = {
     format: 'DD.MM.YYYY, HH:mm',
     firstDayOfWeek: 'mo',
--- src/app/modules/order/components/car-model-list/car-model-list.component.ts
+++ src/app/modules/order/components/car-model-list/car-model-list.component.ts
@@ -4,7 +4,7 @@ import { CarService } from '../../../../shared/services/car.service';
 import { Observable } from 'rxjs';
 import { Car, CarCategory } from '../../../../shared/interfaces/car';
 import { untilDestroyed } from 'ngx-take-until-destroy';
-import { InputCheckedElement } from '../../../../shared/interfaces/input';
+import { carTypeInputs } from './modelTypeInputs';
 
 @Component({
   selector: 'app-car-model-list',
@@ -16,12 +16,7 @@ export class CarModelListComponent implements OnInit, OnDestroy {
   @Input() carModelForm: AbstractControl;
   cars: Observable<Car[]>;
   category: CarCategory = 'Все';
-  carTypeInputs: InputCheckedElement[] = [
-    { id: 'all', value: 'Все', labelValue: 'Все модели' },
-    { id: 'economy', value: 'Эконом', labelValue: 'Эконом' },
-    { id: 'premium', value: 'Премиум', labelValue: 'Премиум' },
-  ];
-
+  carTypeInputs = carTypeInputs;
   constructor(private carService: CarService) {}
 
   ngOnInit(): void {
