454320c22e0287efcb555302f14d8fb357f5498c
Sultan <wndrngr13@gmail.com>
2020-11-18 22:12:31 +0400

    refactor: url const

--- src/app/shared/interceptors/car-sharing.interceptor.ts
+++ src/app/shared/interceptors/car-sharing.interceptor.ts
@@ -6,11 +6,13 @@ import { environment } from '../../../environments/environment';
 @Injectable()
 export class CarSharingInterceptor implements HttpInterceptor {
   constructor() {}
+  private readonly redirectURL = 'https://cors-anywhere.herokuapp.com';
+  private readonly backendURL = 'http://api-factory.simbirsoft1.com';
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
     if (request.url.includes('api/db/') && environment.production) {
       const req = request.clone({
-        url: `https://cors-anywhere.herokuapp.com/http://api-factory.simbirsoft1.com/${request.url}`,
+        url: `${this.redirectURL}/${this.backendURL}/${request.url}`,
       });
       return next.handle(req);
     }
