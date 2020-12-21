ce401310687d23eae723d9ef1eeaab61b879aca2
Sultan <wndrngr13@gmail.com>
2020-12-21 22:06:59 +0400

    feat: add spinner, login trigger

--- src/app/modules/admin/auth/authorization.component.ts
+++ src/app/modules/admin/auth/authorization.component.ts
@@ -1,15 +1,9 @@
-import {
-  Component,
-  OnInit,
-  ChangeDetectionStrategy,
-  ViewChild,
-  ElementRef,
-  OnDestroy,
-} from '@angular/core';
+import { Component, OnInit, ChangeDetectionStrategy, OnDestroy } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { AuthorizationService } from '@shared/services/authorization.service';
 import { Router } from '@angular/router';
-import { untilDestroyed } from 'ngx-take-until-destroy';
+import { Subject } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-authorization',
@@ -19,7 +13,9 @@ import { untilDestroyed } from 'ngx-take-until-destroy';
 })
 export class AuthorizationComponent implements OnInit, OnDestroy {
   authorizationForm: FormGroup;
-  @ViewChild('submitBtn') submitBtn: ElementRef;
+  loginTrigger = new Subject<void>();
+  loginObservable = this.loginTrigger.asObservable();
+  showSpinner: boolean;
 
   constructor(
     private formBuilder: FormBuilder,
@@ -32,18 +28,19 @@ export class AuthorizationComponent implements OnInit, OnDestroy {
       username: ['', [Validators.required, Validators.email]],
       password: ['', Validators.required],
     });
+
+    this.loginObservable
+      .pipe(switchMap(() => this.authorizationService.auth(this.authorizationForm.value)))
+      .subscribe((response) => {
+        this.authorizationService.setToken(response);
+        this.showSpinner = true;
+        this.router.navigate(['/admin']);
+      });
   }
 
   login(): void {
-    this.submitBtn.nativeElement.disabled = true;
-    this.authorizationService
-      .auth(this.authorizationForm.value)
-      .pipe(untilDestroyed(this))
-      .subscribe((response) => {
-        this.authorizationService.setToken(response);
-        this.submitBtn.nativeElement.disabled = false;
-        this.router.navigate(['/admin']);
-      });
+    this.showSpinner = true;
+    this.loginTrigger.next();
   }
 
   ngOnDestroy(): void {}
